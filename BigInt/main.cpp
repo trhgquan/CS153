bca6315024e5abb1fd36276b9ffbb57fd495ecbb
trhgquan <quanthcstpbt@gmail.com>
2022-06-21 11:00:09 +0700

    Remove unneeded comments

--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -1,21 +1,10 @@
-/**
- * @file main.cpp
- * @author Lê Hoàng Trọng Tín (19120682), Lê Mai Nguyên Thảo (19120661), Trần Hoàng Quân (19120338)
- * @brief Thư viện BigInt
- * @version 0.1
- * @date 2022-03-08
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-
 #include"BigInt.h"
 
 int main() {
     try {
-        BigInt m; std::cout << "Moi thay nhap m = "; std::cin >> m;
-        BigInt a; std::cout << "Moi thay nhap a = "; std::cin >> a;
-        BigInt b; std::cout << "Moi thay nhap b = "; std::cin >> b;
+        BigInt m; std::cout << "m = "; std::cin >> m;
+        BigInt a; std::cout << "a = "; std::cin >> a;
+        BigInt b; std::cout << "b = "; std::cin >> b;
 
         std::cout << "(a + b) % m = " << BigInt::modularAddition(a, b, m) << '\n';
         std::cout << "(a * b) % m = " << BigInt::modularMultiplication(a, b, m) << '\n';
--- MillerRabin/MillerRabin.h
+++ MillerRabin/MillerRabin.h
@@ -7,15 +7,40 @@
 
 class MillerRabin {
 private:
+    /**
+     * @brief Base to test.
+     * 
+     */
     std::vector<longint> _testSeed;
 private:
+    /**
+     * @brief Run Miller-Rabin test
+     * 
+     * @return true if number is Prime in this base.
+     * @return false otherwise
+     */
     bool _test(longint, longint);
 public:
+    /**
+     * @brief Power mod
+     * 
+     * @param a 
+     * @param b 
+     * @param n 
+     * @return (a ^ b) % n 
+     */
     static longint powmod(longint, longint, longint);
 public:
     MillerRabin();
     MillerRabin(const std::vector<longint>&);
     ~MillerRabin();
+
+    /**
+     * @brief Run Miller-Rabin test
+     * 
+     * @return true if number is Prime
+     * @return false otherwise
+     */
     bool test(longint);
 };
 
